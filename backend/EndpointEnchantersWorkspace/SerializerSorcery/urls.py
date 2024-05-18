b2f9f60ecff611a0c967513ab1ad31cba0be2494
adamova47 <adamova.ema@gmail.com>
2024-05-19 01:16:29 +0200

    Add urls

--- backend/EndpointEnchantersWorkspace/SerializerSorcery/urls.py
+++ backend/EndpointEnchantersWorkspace/SerializerSorcery/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (AdminCncHomeNavbar, AdminCncHomeText, AdminBibtexChars,
                     AdminCogSciHomeNavbar, AdminCogSciHomeText, AdminAiSeminar,
-                    AdminCncProjects, AdminUserList)
+                    AdminCncProjects, AdminUserList, AdminMyHomeData, AdminMyHomeNavbar, AdminGetInsertData,
+                    AdminProjectsList)
 from .views import CncNavbar, CncFooter, CncGetHtmlContentByName, CncExportBib, Login, Logout
 
 router = DefaultRouter()
@@ -12,11 +13,14 @@ router.register(r'cnc/navbar', CncNavbar, basename='cnc-navbar')
 router.register(r'cnc/footer', CncFooter, basename='cnc-footer')
 
 router.register(r'admin/cnchome', AdminCncHomeNavbar, basename='admin-cnc-navbar')
+router.register(r'admin/cnchometext', AdminCncHomeText, basename='admin-cnchome-text')
 router.register(r'admin/cogscihome', AdminCogSciHomeNavbar, basename='admin-cogsci-navbar')
+router.register(r'admin/cogscitext', AdminCogSciHomeText, basename='admin-cogsci-text')
 router.register(r'admin/bibtexchars', AdminBibtexChars, basename='admin-bibtex-chars')
 router.register(r'admin/aiseminar', AdminAiSeminar, basename='admin-aiseminar')
 router.register(r'admin/projects', AdminCncProjects, basename='admin-projects')
-router.register(r'admin/users', AdminUserList, basename='admin-users')
+router.register(r'admin/userslist', AdminUserList, basename='admin-userslist')
+router.register(r'admin/projectslist', AdminProjectsList, basename='admin-projectslist')
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -27,6 +31,9 @@ urlpatterns = [
     path('admin/wslogin/', Login.as_view(), name='admin-login'),
     path('admin/wslogout/', Logout.as_view(), name='admin-logout'),
 
-    path('admin/cnchometext/<str:getname>/', AdminCncHomeText.as_view(), name='admin-cnc-home-text'),
-    path('admin/cogscitext/<str:getname>/', AdminCogSciHomeText.as_view(), name='admin-cog-sci-text'),
+    path('admin/myhomenavbar/', AdminMyHomeNavbar.as_view(), name='admin-myhomenavbar'),
+    path('admin/myhomedata/', AdminMyHomeData.as_view(), name='admin-myhomedata'),
+
+    path('admin/insertdata/', AdminGetInsertData.as_view(), name='admin-insertdata'),
+    path('admin/insertdata/<str:username>', AdminGetInsertData.as_view(), name='admin-insertdata-user'),
 ]
